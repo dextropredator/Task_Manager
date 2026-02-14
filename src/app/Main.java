82c951f1457a2b42222b03f573b65e72eb8d6fd7
dextropredator <kumar292715@gmail.com>
2026-02-14 22:17:25 +0530

    4

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,7 +6,7 @@ class Main{
     public static void main(String[] args) {
         Task t1 = new Task("Test", "09/02/2026", 1) ;
         t1.show();
-        t1.setcompletionDate("14/02/2026");
+        t1.setCompletionDate("14/02/2026");
         t1.show();
         t1.completedTask("09/03/2026");
         t1.show();
